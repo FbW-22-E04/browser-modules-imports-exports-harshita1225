0b7ed8d51889cfae24ea8bb399b9f9148b7b5e4f
harshita <harshita1225@gmail.com>
2022-09-26 15:46:43 +0200

    task completed

--- modules/index.js
+++ modules/index.js
@@ -1,1 +1,11 @@
 // Your code goes here!
+
+import { fixRoundingErrors, calculateAspectRatio } from "./aspect-ratio.js";
+import { modulo, percentage, percentageOf, difference } from "./percentage.js";
+
+const m1 = document.querySelector("#module_1");
+const m2 = document.querySelector("#module_2");
+const mresult = document.querySelector("#module_result");
+
+function modulo() {}
+m1.addEventListener("input", modulo);
